e43a7cbe774270e7a04e5e0367ada8801df1a751
Daniel Dimovski <daniel.k.dimovski@gmail.com>
2020-11-15 13:31:52 +0100

    CLI handling and writing to disk/stdout

---
+++ src/ggpk.rs
@@ -0,0 +1,117 @@
+use memmap::MmapOptions;
+use std::io::{Read, SeekFrom, Seek, Cursor};
+use std::fs::File;
+use byteorder::{LittleEndian, ReadBytesExt};
+use rayon::prelude::*;
+use std::convert::TryFrom;
+use std::error::Error;
+use memmap::Mmap;
+use std::collections::HashMap;
+use std::collections::LinkedList;
+
+use super::shared::GGPK;
+use super::shared::FileRecord;
+
+pub fn read<F>(filepath: &str, callback: F) where F: Fn(GGPK) {
+    let file = File::open(filepath).expect("Opening GGPK file");
+    let mmap = unsafe { MmapOptions::new().map(&file).expect("Mapping GGPK file to memory") };
+    let files_list = read_record(&mmap, 0, &String::from(""));
+
+    let mut files = HashMap::new();
+    files_list.iter().for_each(|record| {
+        files.insert(format!("{}/{}", record.path, record.name), record);
+    });
+
+    callback(GGPK { mmap, files });
+}
+
+fn read_record(mmap: &Mmap, offset: u64, base: &String) -> LinkedList<FileRecord> {
+    let mut c = Cursor::new(mmap);
+    c.set_position(offset);
+
+    let record_size = c.read_u32::<LittleEndian>().unwrap();
+    let record_type = read_record_tag(&mut c).unwrap();
+
+    trace!("RECORD {} offset({}) {} bytes", record_type, offset, record_size);
+    match record_type.as_str() {
+        "GGPK" => {
+            let ggpk_version = c.read_u32::<LittleEndian>().unwrap();
+            trace!("GGPK version {}", ggpk_version);
+
+            let records = (record_size - 12) / 8;
+            return (0..records)
+                .map(|_| c.read_u64::<LittleEndian>().unwrap())
+                .map(|offset| read_record(mmap, offset, &base))
+                .fold(LinkedList::new(), |mut acc, mut x| {acc.append(&mut x); acc} );
+        },
+        "PDIR" => {
+            c.seek(SeekFrom::Current(4)).unwrap(); // ignore name length
+            let entries_length = c.read_u32::<LittleEndian>().unwrap();
+            c.seek(SeekFrom::Current(32)).unwrap(); // ignore hash value
+            let name = read_utf16(&mut c);
+            let path = if base.len() == 0 { name.clone() } else { format!("{}/{}", base, name) };
+
+            return (0..entries_length)
+                .into_iter()    
+                .into_par_iter()
+                .map(|i| {
+                    let mut c2 = Cursor::new(&mmap);
+                    c2.set_position(c.position() + (i * 12) as u64);
+                    c2.seek(SeekFrom::Current(4)).unwrap(); // ignore hash value
+                    let offset = c2.read_u64::<LittleEndian>().unwrap();
+                    return read_record(mmap, offset, &path);
+                })
+                .fold(|| LinkedList::new(), |mut acc, mut x| {acc.append(&mut x); acc} )
+                .reduce(|| LinkedList::new(), |mut acc, mut x| {acc.append(&mut x); acc} );
+        },
+        "FILE" => {
+            let name_length = c.read_u32::<LittleEndian>().unwrap();
+            let signature = read_file_signature(&mut c).unwrap();
+            let filename = read_utf16(&mut c);
+            let name_size = (filename.len() + 1) * 2;
+            if usize::try_from(name_length).unwrap() != filename.len() + 1 {
+                warn!("Length of '{}' different than specified. Expected: {}", filename, name_length);
+            }
+            
+            let data_start = usize::try_from(c.position()).unwrap();
+            let data_size = record_size - 44 - name_size as u32; // 44 = length + tag + strlen + hash
+            
+            let mut list = LinkedList::new();
+            list.push_back(FileRecord {
+                name: filename,
+                path: format!("{}", base),
+                signature,
+                begin: data_start,
+                bytes: data_size,
+            });
+            return list;
+        }
+        "FREE" => 
+             LinkedList::new(), // Unused space, ignore
+        _ => {
+            warn!("Found undefined type: {}", record_type);
+            return LinkedList::new();
+        }
+    }
+}
+
+fn read_record_tag(c: &mut Cursor<&memmap::Mmap>) -> Result<String, Box<dyn Error>> { 
+    let mut bytes = [0u8; 4];
+    c.read_exact(&mut bytes)?;
+    let tag = String::from_utf8((&bytes).to_vec())?;
+    return Ok(tag);
+}
+
+fn read_file_signature(c: &mut Cursor<&memmap::Mmap>) -> Result<[u8; 32], Box<dyn Error>> { 
+    let mut bytes = [0u8; 32];
+    c.read_exact(&mut bytes)?;
+    return Ok(bytes);
+}
+
+fn read_utf16(c: &mut Cursor<&memmap::Mmap>) -> String {
+    let raw = (0..)
+        .map(|_| c.read_u16::<LittleEndian>().unwrap())
+        .take_while(|&x| x != 0u16)
+        .collect::<Vec<u16>>();
+    return String::from_utf16(&raw).unwrap();
+}
--- src/main.rs
+++ src/main.rs
@@ -1,89 +1,120 @@
-use memmap::MmapOptions;
-use std::io::{Read, Write, SeekFrom, Seek, Cursor};
-use std::fs::{File, create_dir};
-use byteorder::{LittleEndian, ReadBytesExt};
-use rayon::prelude::*;
-use std::time::Instant;
+mod ggpk;
+use std::path::Path;
+use std::fs::{File, create_dir_all};
+use std::io::{self, Write};
 use std::convert::TryFrom;
 
-fn read_record_tag(rdr: &mut Cursor<&memmap::Mmap>) -> String {
-    let mut tag = [0u8; 4];
-    rdr.read_exact(&mut tag).unwrap();
-    return String::from_utf8((&tag).to_vec()).expect("Reading UTF-8 record tag");
+#[macro_use] extern crate log;
+extern crate simplelog;
+use simplelog::*;
+use clap::{Arg, App};
+use memmap::Mmap;
+
+mod shared;
+use shared::FileRecord;
+
+fn main() {
+    let matches = App::new("GGPK Reader")
+        .version("1.0")
+        .author("Daniel D. <daniel.k.dimovski@gmail.com>")
+        .about("Reads the GGPK fileformat from the game Path of Exile")
+        .arg(Arg::with_name("path")
+            .short("p")
+            .long("path")
+            .value_name("DIRECTORY")
+            .help("Specify location of Path of Exile installation")
+            .required_unless("file")
+            .takes_value(true))
+        .arg(Arg::with_name("file")
+            .short("f")
+            .long("file")
+            .value_name("FILE")
+            .help("Specify location of GGPK file")
+            .required_unless("path")
+            .takes_value(true))
+        .arg(Arg::with_name("query")
+            .short("q")
+            .long("query")
+            .value_name("QUERY")
+            .help("Filter output to include provided substring")
+            .takes_value(true))
+        .arg(Arg::with_name("output")
+            .short("o")
+            .long("output")
+            .value_name("DIRECTORY")
+            .help("Write files to location")
+            .takes_value(true))
+        .arg(Arg::with_name("silent")
+            .short("s")
+            .long("silent")
+            .help("Prevent writing to stdout"))
+        .arg(Arg::with_name("binary")
+            .short("b")
+            .long("binary")
+            .help("Output file contents to stdout"))
+        .arg(Arg::with_name("v")
+            .short("v")
+            .multiple(true)
+            .help("Sets the level of verbosity"))
+        .get_matches();
+
+    CombinedLogger::init(
+        vec![
+            TermLogger::new(LevelFilter::Warn, Config::default(), TerminalMode::Mixed)
+        ]
+    ).unwrap();
+
+    let query = matches.value_of("query").unwrap_or("");
+    let silent = matches.is_present("silent");
+
+    let file = if matches.is_present("file") {
+        matches.value_of("file").unwrap().to_string()
+    } else {
+        format!("{}/Content.ggpk", matches.value_of("path").unwrap())
+    };
+
+    ggpk::read(file.as_str(), |ggpk| {
+        ggpk.files.iter()
+            .filter(|(filepath, _)| query.is_empty() || filepath.contains(query) )
+            .map(|tuple| tuple.1)
+            .for_each(|record| {
+
+                if let Some(output) = matches.value_of("output") {
+                    let path = format!("{}/{}/{}", output, record.path, record.name);
+
+                    if !silent { println!("Writing {}", path) }
+                    create_path(path.as_str());
+                    read_record(record, &ggpk.mmap, |bytes| {
+                        File::create(path.as_str()).unwrap().write(bytes).unwrap();
+                    });
+
+                } else if matches.is_present("binary") {
+                    read_record(record, &ggpk.mmap, |bytes| {
+                        io::stdout().write_all(bytes).unwrap();
+                    });
+
+                } else {
+                    println!("{}/{}", record.path, record.name)
+                }
+            });
+    });
 }
 
-fn read_utf16(rdr: &mut Cursor<&memmap::Mmap>) -> String {
-    let raw = (0..)
-        .map(|_| rdr.read_u16::<LittleEndian>().expect("Read UTF-16 until NULL term"))
-        .take_while(|&x| x != 0u16)
-        .collect::<Vec<u16>>();
-    return String::from_utf16(&raw).expect("Decode a UTF-16 String")
-}
-
-fn read_record(mmap: &memmap::Mmap, offset: u64, base: &String) {
-    let mut rdr = Cursor::new(mmap);
-    rdr.set_position(offset);
-
-    let byte_size = rdr.read_u32::<LittleEndian>().unwrap();
-    let record_type = read_record_tag(&mut rdr);
-    match record_type.as_str() {
-        "GGPK" => { // Root record of the file.
-            let records = rdr.read_u32::<LittleEndian>().unwrap();
-            (0..records)
-                .map(|_| rdr.read_u64::<LittleEndian>().unwrap())
-                .for_each(|offset| read_record(mmap, offset, &base));
+fn create_path(path: &str) {
+    match Path::new(path).parent() {
+        Some(directory) => {
+            let _ = create_dir_all(directory);
         },
-        "PDIR" => {
-            rdr.seek(SeekFrom::Current(4)).unwrap(); // ignore name length
-            let entries_length = rdr.read_u32::<LittleEndian>().unwrap();
-            rdr.seek(SeekFrom::Current(32)).unwrap(); // ignore hash value
-            let name = read_utf16(&mut rdr);
-            let path = if base.len() == 0 { name.clone() } else { format!("{}/{}", base, name) };
-            if path.len() > 0 {
-                let _ = create_dir(format!("dump/{}", path.clone()));
-            }
-
-            (0..entries_length)
-                .into_par_iter()
-                .for_each(|i| {
-                    let mut rdr2 = Cursor::new(mmap);
-                    rdr2.set_position(rdr.position() + (i * 12) as u64);
-                    rdr2.seek(SeekFrom::Current(4)).unwrap(); // ignore hash value
-                    let offset = rdr2.read_u64::<LittleEndian>().unwrap();
-                    read_record(mmap, offset, &path);
-                });
-        },
-        "FILE" => {
-            rdr.seek(SeekFrom::Current(4)).unwrap(); // ignore name length
-            rdr.seek(SeekFrom::Current(32)).unwrap(); // ignore hash value
-            let name = read_utf16(&mut rdr);
-            let name_size = (name.len()+1) * 2;
-            
-            let path = if base.len() == 0 { name.clone() } else { format!("{}/{}", base, name) };
-            let data_start = usize::try_from(rdr.position()).unwrap();
-            let data_length = byte_size - 44 - name_size as u32; // 44 = length + tag + strlen + hash
-            let data_end = data_start + usize::try_from(data_length).unwrap();
-            
-            match mmap.get(data_start..data_end) {
-                Some(bytes) => {
-                    if name.ends_with(".txt") {
-                        File::create(format!("dump/{}", path.clone())).unwrap()
-                        .write(bytes).unwrap();
-                    }
-                },
-                None    => println!("Failed getting slice of file {} {}->{}", name, data_start, data_end),
-            }
-        }
-        _ => println!("Record type {0} not implemented.", record_type),
+        None => {},
     }
 }
 
-fn main() {
-    let now = Instant::now();
-
-    let file = File::open("/mnt/d/Path of Exile/Content_old.ggpk").unwrap();
-    let mmap = unsafe { MmapOptions::new().map(&file).unwrap() };
-    read_record(&mmap, 0, &String::from(""));
-    
-    println!("Time spent: {}ms", now.elapsed().as_millis());
+pub fn read_record<F>(record: &FileRecord, mmap: &Mmap, callback: F) where F: Fn(&[u8]) {
+    let file_end = record.begin + usize::try_from(record.bytes).unwrap();
+    match mmap.get(record.begin..file_end) {
+        Some(bytes) => {
+            callback(bytes);
+        },
+        None => error!("Failed reading bytes for file {}/{}", record.path, record.name),
+    }
 }
